49e5a4634d90e569c4bb1ac7cc5b294acb09cd11
chloe0x0 <ChloeZeiter@gmail.com>
2023-12-01 15:13:21 -0800

    Application functional.

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # cwon
 A CLI for generating CRON expressions from natrual english phrases in Go. 
 
-
 ## Building 
 
 ```console
 go build
+```
+
+## Usage
+```console
+cwon At midnight on christmas
+0 0 0 25 12 *
 ```
--- main.go
+++ main.go
@@ -3,10 +3,49 @@ package main
 import (
 	"fmt"
 	"os"
+	"io"
+	"encoding/json"
+	"strings"
+	"net/http"
 )
+
+const (
+	// where we make GET requests for our CRON expressions
+	base string = "https://cronprompt.com/api/prompt?prompt="
+	usage string = "cwon [expression: string]. Example: cwon At midnight on Christmas"
+)
+
+type response struct {
+	Cron string `json:"result"`
+	Call string `json:"call"`
+}
 
 func main() {
 	// shift the args to remove the program
-	prompt := os.Args[1:]
-	fmt.Println("prompt: ", prompt)
+	prompt := strings.Join(os.Args[1:], " ")
+	if len(prompt) == 0 {
+		fmt.Println("No expression provided.")
+		fmt.Println(usage)
+		os.Exit(0)
+	}
+	// construct the URL to which we will make our GET request
+	url := base + prompt
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	data := response{}
+	if err := json.Unmarshal(body, &data); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(data.Cron)
 }
