fe39b498669741a347a9335a10198766c7816d26
chloe0x0 <ChloeZeiter@gmail.com>
2023-12-01 21:56:33 -0800

    Status Code check

--- main.go
+++ main.go
@@ -35,17 +35,24 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	defer resp.Body.Close()
 
-	// Read the response body into a byte array so we can unmarshal it later
+	// Read the response body into a byte array so we can unmarshal i later
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
 
+	if resp.StatusCode > 299 {
+		fmt.Printf("Response failed with status code: %d and\nbody: %s\n", resp.StatusCode, body)
+		os.Exit(1)
+	}
+
 	// Parse the JSON response into a response struct
 	data := response{}
 	if err := json.Unmarshal(body, &data); err != nil {
+		fmt.Println(string(body))
 		panic(err)
 	}
 
